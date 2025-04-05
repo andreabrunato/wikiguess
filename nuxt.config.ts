d75d9cd294d651f429d692f0426ce0b615881fb4
Br1 <andrea.brunato@live.com>
2025-04-05 18:30:30 +0200

    gh-pages

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -3,10 +3,9 @@ import { resolve } from 'path';
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
+  target: 'static', // Imposta la modalità statica
   router: {
-    options: {
-      // Rimuovo la proprietà `base` non valida
-    }
+    base: '/wikiguess/' // Base path per GitHub Pages
   },
   generate: {
     routes: ['/', '/about'] // Sostituisce fallback con una configurazione valida
